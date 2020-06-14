46a7269cf57e8c905a81aed0d7ff3ebd8f184388
yash001dev <y.sanghavi001@gmail.com>
2020-06-14 04:04:20 -0700

    routing with tab

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -5,16 +5,54 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
-    path: '',
-    component: PlacesPage
+    path: 'tabs',
+    component: PlacesPage,
+    children:[
+      {
+        path:'discover',
+        children:[
+          {
+            path:'',
+            loadChildren:()=>import('./discover/discover.module').then(m=>m.DiscoverPageModule)
+          },
+          {
+            path:':placeId',
+            loadChildren:()=>import('./discover/place-detail/place-detail.module').then(m=>m.PlaceDetailPageModule)
+          }
+        ]
+      },
+      {
+        path: 'offers',
+        children:[
+          {
+            path:'',
+            loadChildren:()=>import('./offers/offers.module').then(m=>m.OffersPageModule)
+          },
+          {
+            path:'new',
+            loadChildren:()=>import('./offers/new-offer/new-offer.module').then(m=>m.NewOfferPageModule)
+          },
+          {
+            path:'edit/:placeId',
+            loadChildren:()=>import('./offers/edit-offer/edit-offer.module').then(m=>m.EditOfferPageModule)
+          },
+          {
+            path:':placeId',
+            loadChildren:()=>import('./offers/offer-bokkings/offer-bokkings.module').then(m=>m.OfferBokkingsPageModule)
+          }
+        ]
+      },
+      {
+        path:'',
+        redirectTo:'/places/tabs/discover',
+        pathMatch:'full'
+      }
+    ]
   },
   {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+    path:'',
+    redirectTo:'/places/tabs/discover',
+    pathMatch:'full'
   }
 ];
 
